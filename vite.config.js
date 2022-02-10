040d90469af195ddbea92edeac918d87f868ce9f
leire <leire.maza@alumni.mondragon.edu>
2022-02-10 11:05:18 +0100

    conf changed

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import 'dotenv/config'
 
 const env = require("dotenv").config();
-console.log(process.env)
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/" + "acm_chat"  + "/",
+  base: "/" + process.env.GIT_REPO  + "/",
 })
